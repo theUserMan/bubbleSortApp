0382d6b4dd44af0d4b6d6deae5c629643accadb8
Nathan Cobb <nathanrcobb@gmail.com>
2017-09-20 19:23:40 -0700

    Latest Version

--- bubbleSortApp/app/functions.py
+++ bubbleSortApp/app/functions.py
@@ -1,34 +1,25 @@
-# Test the algorithm
-def testSort(arr,cur):
-    return arr[cur] == sorted(arr[cur])
-
 # Bubble up lesser element
 def bubbleUp(arr, cur, end):
-    for i in range(end,cur,-1):
-        if arr[cur-1][i] < arr[cur-1][i-1]:
-            arr[cur] += [i]
-            arr[cur][i-1] = arr[cur-1][i]
-            arr[cur][i] = arr[cur-1[i-1]]
+    for i in range(end-1,cur-1,-1):
+        if arr[cur][0][i+1] < arr[cur][0][i]:
+            arr += [[arr[cur][0],i]]
+            arr[cur][0][i+1], arr[cur][0][i] = arr[cur][0][i], arr[cur][0][i+1]
 
 # Sort the list
-def bubbleSort(arr,cur):
-    numElts = len(arr[cur])
-    while not(cur == numElts):
-        arr += [arr[cur],-1]
-        cur += 1
-        bubbleUp(arr,cur,numElts-1)
-    return arr
+def bubbleSort(arr, cur):
+    end = len(arr[cur][0])-1
+    for i in range(end):
+        bubbleUp(arr,i,end)
 
 # Main
-def bubbleMain(arr):
-    cur = 0
-    arr = [[a for a in arr.split()],0]
-    print("Sorting "+str(arr[0]))
+def bubbleMain(inputArr):
+    arr = [[[int(a) for a in inputArr.split()],-1]]
+    size = len(arr[0][0])
 
-    if len(arr[0])>1 and len(arr[0])<9:
-        arr = bubbleSort(arr,cur)
+    if size>1 and size<9:
+        bubbleSort(arr,0)
 
-    if testSort(arr[len(arr[0])-1], cur):
+    if arr[size-2][0] == sorted(arr[0][0]):
         return arr
     else:
         return ""
