34487f9e14a82e4d12f34693fa8b771d4c004ed6
Nathan Cobb <nathanrcobb@gmail.com>
2017-09-21 08:16:35 -0700

    Adding alerts

--- bubbleSortApp/static/app/scripts/general.js
+++ bubbleSortApp/static/app/scripts/general.js
@@ -1,41 +1,37 @@
 function launchBubbleSortAlg() {
     var inputArray = $('#inputArray').val();
-    var table = document.getElementById("resultTable");
-    document.getElementById("launchButton").disabled = true;
-    document.getElementById("quitButton").disabled = false;
-    $.ajax({
-        url: "launchBubble",
-        data: {
-            'inputArray': inputArray
-        },
-        success: function (data) {
-            //alert(data);
-            //alert('Your input array: \n' + inputArray);
-            var row = table.insertRow(2);
-            var cell1 = row.insertCell(0);
-            var cell2 = row.insertCell(1);
-            var parsed = JSON.parse(data);
-            for (var i = parsed.length - 1; i >= 0; i--) {
-                cell1.innerHTML = i;
-                /*var j = 0;
-                for (var w in parsed[i]) {
-                    cell2.innerHTML = w;
-                    if (j == parsed[i][1] || j == parsed[i][1] + 1)
-                        cell2.innerHTML = "<span style=\"color:red\">" + w;
-                    else
-                        cell2.innerHTML = w;
-                    j++;
-                }*/
-                cell2.innerHTML = parsed[i][0];
-                row = table.insertRow(2);
-                cell1 = row.insertCell(0);
-                cell2 = row.insertCell(1);
+    if inputArray.length > 1 && inputArray.length < 9 {
+        var table = document.getElementById("resultTable");
+        document.getElementById("launchButton").disabled = true;
+        document.getElementById("quitButton").disabled = false;
+        $.ajax({
+            url: "launchBubble",
+            data: {
+                'inputArray': inputArray
+            },
+            success: function (data) {
+                //alert(data);
+                //alert('Your input array: \n' + inputArray);
+                var row = table.insertRow(2);
+                var cell1 = row.insertCell(0);
+                var cell2 = row.insertCell(1);
+                var parsedData = JSON.parse(data);
+                for (var i = 0; i < data.length; i++) {
+                    cell1.innerHTML = i;
+                    cell2.innerHTML = "<span style=\"color:red\">" + data;
+                    row = table.insertRow(2);
+                    cell1 = row.insertCell(0);
+                    cell2 = row.insertCell(1);
+                }
+            },
+            error: function (data) {
+                alert("Bad Data");
             }
-        },
-        error: function (data) {
-            alert("failed");
-        }
-    })
+        })
+    }
+    else {
+        alert("Invalid Input")
+    }
 }
 function quitBubbleSortAlg() {
     var table = document.getElementById("resultTable");
